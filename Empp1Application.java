652816180c11c682790b157e08d47faba661504e
DHANYAPRIYA <180368907+Dhanyapriya15@users.noreply.github.com>
2025-01-28 00:06:18 +0530

    Create Empp1Application.java

---
+++ Empp1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Empp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Empp1Application.class, args);
+	}
+}
